321b648305f9c22e81fc4e5fe8ce67fce4f577b4
Gerry McLarnon <gerry.mclarnon@beatsystems.com>
2015-01-21 17:01:50 +0000

    Added simple firebase login

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -25,6 +25,23 @@ export default Ember.ArrayController.extend({
     },
     destroyUser: function(model) {
       model.destroyRecord();
+    },
+    onLoginButtonClicked: function(model) {
+        var commentsRef = new Firebase('https://playermanager.firebaseio.com/teams/broomhillrovers');
+//Create an Firebase Simple Login client so we can do Facebook auth
+        var auth = new FirebaseSimpleLogin(commentsRef, function(error, user) {
+            if (error) {
+                // an error occurred while attempting login
+                console.log(error);
+            } else if (user) {
+//                myUserID = user.id;
+                $("#loginDiv").text(user.first_name + " " + user.last_name + " (" + user.email + ")");
+            } else {
+                // User logged out
+            }
+        });
+
+        auth.login("password", { email: "admin@playermanager.net", password: "thegaffer", rememberMe: false });
     }
   }
 });
