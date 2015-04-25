2f5f74ac1d561ca9b6a3c932566e2d7e8247aa05
gerrym <gerry.mclarnon@beatsystems.com>
2015-04-25 13:28:03 +0100

    Added Facebook login.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -17,6 +17,20 @@ export default Ember.Controller.extend({
                     controller.transitionToRoute('players');
                 }
             });
-        }
+        },
+
+        loginFacebook: function(username, password) {
+            var controller = this;
+            this.firebase.authWithOAuthPopup("facebook", function (error, authData) {
+                if (error) {
+                    console.log("Login Failed!", error);
+                    controller.set('errorMessage', error.message);
+                } else {
+                    console.log("Authenticated successfully with payload:", authData);
+                    controller.transitionToRoute('players');
+                }
+            });
+        },
+
     }
 });
