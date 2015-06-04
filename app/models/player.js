67fc76506ed88f94c5884c083d78d949fe6ba4fe
Gerry McLarnon <gerry.mclarnon@beatsystems.com>
2015-06-04 16:12:46 +0100

    http://ryanflorence.com/jsconf-avatars/avatars/ryan.jpg doesn't cause flicker but this PNG does, http://placehold.it/42x42

--- app/models/player.js
+++ app/models/player.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
-  lastName: DS.attr('string')
+  lastName: DS.attr('string'),
+  avatar: DS.attr('string', { defaultValue: 'http://ryanflorence.com/jsconf-avatars/avatars/ryan.jpg'})
 });
