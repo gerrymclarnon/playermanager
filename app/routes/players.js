7cbc222d5cc86860d09d509df01b72a2c62a3e65
Gerry McLarnon <gerry.mclarnon@beatsystems.com>
2015-06-10 16:59:51 +0100

    Updated edit icons

--- app/routes/players.js
+++ app/routes/players.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function() {
         return this.store.find('player');
+    },
+    
+    actions: {
+        showSquad: function () {
+            this.get('controller').send('showMenu');
+            this.transitionTo('players');
+        },
+        
+        logout: function () {
+            this.transitionTo('login');
+        }
     }
 });
