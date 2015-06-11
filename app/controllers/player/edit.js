5e4c8bc77fdfcd0b881ebcad8dcaf94f29782f66
gerrym <gerry.mclarnon@beatsystems.com>
2015-06-11 21:44:23 +0100

    Fixed post delete transition

--- app/controllers/player/edit.js
+++ app/controllers/player/edit.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    needs: ['players', 'player']
-
+    actions: {
+        cancel: function(player) {
+            player.rollback();
+            this.transitionTo('player');
+        },
+        update: function(player) {
+            player.save();
+            this.transitionTo('player');
+        },
+        delete: function(player) {
+            player.destroyRecord();
+            this.transitionTo('players');
+        }
+    }
 });
