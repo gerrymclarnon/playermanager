d5412f2eb758ba17288bb7b83f3ffd3c46d0844e
gerrym <gerry.mclarnon@beatsystems.com>
2015-06-17 14:59:38 +0100

    Merge branch 'master' of https://github.com/gerrymclarnon/playermanager
    Conflicts:
    	.idea/workspace.xml

---
+++ app/components/secondary-header-bar.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    classNames: ['bar', 'bar-standard', 'bar-header-secondary'],
+    classNameBindings: ['showSecondaryHeader:show', 'showMenu:showMenu'],
+    showSecondaryHeader: false
+});
