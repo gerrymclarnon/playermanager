9dbe5228689b0665443c9fff0fd03062b9c721ee
Gerry McLarnon <gerry.mclarnon@beatsystems.com>
2015-06-23 16:59:34 +0100

    Added editable player-info

--- app/controllers/player/edit.js
+++ app/controllers/player/edit.js
@@ -9,18 +9,12 @@ export default Ember.Controller.extend({
             this.transitionTo('player');
         },
         update: function(player) {
-            var info = player.get('info');
-            
-            if (Ember.isNone(info)) {
-                info = this.store.createRecord('player-info', {
-                    appearances: 10,
-                    goals: 20
-                });
-            
-                player.set('info', info);
-            }
-            
-            player.save();
+            var info = player.get('info.content');
+
+            info.save().then(function() {
+                player.save();
+            });
+
             this.transitionTo('player');
         },
         delete: function(player) {
