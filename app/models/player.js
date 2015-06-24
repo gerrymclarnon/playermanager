341d00ab8044ba18d7c064a651f9182bd33ea9f0
Gerry McLarnon <gerry.mclarnon@beatsystems.com>
2015-06-24 11:15:17 +0100

    Adding more info to player.

--- app/models/player.js
+++ app/models/player.js
@@ -3,6 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
+  fullName: Ember.computed('firstName', 'lastName', function () {
+        return this.get('firstName') + ' ' + this.get('lastName');
+  }),
   avatar: DS.attr('string', { defaultValue: 'http://ryanflorence.com/jsconf-avatars/avatars/ryan.jpg'}),
   info: DS.belongsTo('player-info', { async: true })
 });
