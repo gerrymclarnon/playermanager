b976044c59c15b1611dfb549b60e813174e665b5
Gerry McLarnon <gerry.mclarnon@beatsystems.com>
2015-07-09 16:05:14 +0100

    Added camera support

--- app/models/player.js
+++ app/models/player.js
@@ -2,11 +2,12 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  fullName: Ember.computed('firstName', 'lastName', function () {
+    firstName: DS.attr('string'),
+    lastName: DS.attr('string'),
+    fullName: Ember.computed('firstName', 'lastName', function () {
         return this.get('firstName') + ' ' + this.get('lastName');
-  }),
-  avatar: DS.attr('string', { defaultValue: 'http://ryanflorence.com/jsconf-avatars/avatars/ryan.jpg'}),
-  info: DS.belongsTo('player-info', { async: true })
+    }),
+    photo: DS.attr('string'),
+    avatar: DS.attr('string', { defaultValue: 'http://ryanflorence.com/jsconf-avatars/avatars/ryan.jpg'}),
+    info: DS.belongsTo('player-info', { async: true })
 });
